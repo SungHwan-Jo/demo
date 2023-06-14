be6be371a10533d07b8e37f2cacba7fc341b3e21
sujeonglee4 <sujeong_lee4@tmaxsoft.co.kr>
2023-06-14 09:42:19 +0900

    redis 연동 및 session redis 사용 설정 추가 redis session 저장 확인

--- src/main/java/com/tmax/demo/controller/LoginController.java
+++ src/main/java/com/tmax/demo/controller/LoginController.java
@@ -2,17 +2,42 @@ package com.tmax.demo.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class LoginController {
     // Log4j Logger setting
     private final Logger logger = LogManager.getLogger(LoginController.class);
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
     @GetMapping("/login")
     public String login(){
 
         logger.warn("### Login Controller ###");
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginWithEmail(LoginForm form, HttpSession session) {
+        UUID uid = Optional.ofNullable(UUID.class.cast(session.getAttribute("uid")))
+                .orElse(UUID.randomUUID());
+        session.setAttribute("uid", uid);
+        return "redirect:/main";
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/com/tmax/demo/controller/LoginForm.java
@@ -0,0 +1,17 @@
+package com.tmax.demo.controller;
+
+public class LoginForm {
+
+    private String emailaddress;
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setEmailaddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+
+
+
+
+}
