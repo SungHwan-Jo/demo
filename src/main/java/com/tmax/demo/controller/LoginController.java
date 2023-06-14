9ea50695f7bf6de573c9fc9626774cf0eaa5e493
sujeonglee4 <sujeong_lee4@tmaxsoft.co.kr>
2023-06-14 15:28:39 +0900

    redis에 session 저장 부분 제거

--- src/main/java/com/tmax/demo/controller/LoginController.java
+++ src/main/java/com/tmax/demo/controller/LoginController.java
@@ -4,22 +4,16 @@ import com.tmax.demo.service.LoginService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.Enumeration;
-import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 public class LoginController {
     // Log4j Logger setting
     private final Logger logger = LogManager.getLogger(LoginController.class);
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
 
     private final LoginService loginService;
 
